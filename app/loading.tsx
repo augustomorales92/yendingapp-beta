1d8a7ef96a0782f260e545f4034264e3ca98f187
augustomorales92 <augustomorales92@gmail.com>
2024-07-18 16:43:05 +0200

    update loading file

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { ClipLoader } from 'react-spinners'
 
-const loading = () => {
+export default function Loading(){
     return (
         <div className="flex justify-center m-auto">
         <ClipLoader color="white" size={50} />
       </div>
     );
 };
-
-export default loading;
