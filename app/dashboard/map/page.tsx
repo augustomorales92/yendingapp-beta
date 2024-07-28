f96713ab83492c4cb8e61beb242f1217f69a18da
augustomorales92 <augustomorales92@gmail.com>
2024-07-28 19:12:31 +0200

    add map to the project

--- app/dashboard/map/page.tsx
+++ app/dashboard/map/page.tsx
@@ -1,12 +1,16 @@
 import React, { Suspense } from 'react';
 import Loader from '@/components/Loader';
+import { MapProvider } from '@/lib/MapProvider';
+import Map from '@/components/Map';
 
 export default function Page() {
   return (
     <Suspense fallback={<Loader />}>
-    <div>
-      <h1>Map previas location</h1>
-    </div>
+    <MapProvider>
+      <main className='flex justify-center items-center p-2'>
+        <Map />
+      </main>
+    </MapProvider>
     </Suspense>
   );
 }
